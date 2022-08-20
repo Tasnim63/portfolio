7b6b314a2cccd98d0d8287b9068fe35e2d7f0c07
Tamanna Akter <96432968+Tasnim63@users.noreply.github.com>
2022-08-20 09:12:17 +0600

    button animation added

--- src/pages/projects/AllProjects.jsx
+++ src/pages/projects/AllProjects.jsx
@@ -17,7 +17,7 @@ export default function AllProjects() {
       <h2 className=" mt-4 text-center mb-3   text-white">My Projects</h2>
       <div className="manu_tabs container my-4">
         <div className="menu_tab text-center  ">
-        <a onClick={() => setCards(Menu)} className="btn about_btn  m-2">
+          <a onClick={() => setCards(Menu)} className="btn about_btn  m-2">
             ALL
           </a>
           <a
@@ -44,37 +44,48 @@ export default function AllProjects() {
       {/* main section */}
       <div className="container">
         <div className="row justify-content-center w-auto">
-      
           {cards.map((card) => {
             const { id, image, name, url, details } = card;
             return (
               <>
                 <div className="col-lg-4 col-md-6  my-3 ">
-                  <div style={{background:"#180e2c"}}  class="card  h-100 border-0" >
-                  <div className="image_inner">
-                  <img src={image} class="card-img-top" alt="..." />
-                  </div>
-                    <div  class="card-body">
-                      <h3 style={{color:"#c6b4ea"}}  class="card-title" >{name}</h3>
-                      <p style={{color:"white"}} class="card-text my-3">
-                       {details}
-                      </p>
-                    <div className="card-button d-flex justify-content-between">
-                    <a   className="source_code_btn" href="#" >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                         source_code
-                      </a>
-                      <a  className="live_site_btn" href="#" >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                       live_site
-                      </a>
+                  <div
+                    style={{ background: "#180e2c" }}
+                    class="card  h-100 border-0"
+                  >
+                    <div className="image_inner">
+                      <img src={image} class="card-img-top" alt="..." />
                     </div>
+                    <div class="card-body py-5">
+                      <h3 style={{ color: "#c6b4ea" }} class="card-title">
+                        {name}
+                      </h3>
+                      <p style={{ color: "white" }} class="card-text my-3">
+                        {details}
+                      </p>
+
+                      <div className="button_area py-lg-5 pb-sm-4  ">
+                        <div className=" card_button  ">
+                          <a href="https://github.com/Tasnim63/education-master">
+                            SOURCE_CODE
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                          </a>
+
+                          <a
+                            className="  link_button me-lg-4 "
+                            href="https://hilarious-caramel-99167f.netlify.app/"
+                          >
+                            live_site
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                          </a>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
